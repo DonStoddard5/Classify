cd44416a718d77d8627b1abcc6785fe3cf988ee9
Don <tuj45546@temple.edu>
2019-11-12 18:14:16 -0500

    I did some stuff

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='deepnet',
+    version='1.0',
+    packages=['deepnet'],
+    url='',
+    license='',
+    author='Don Webster',
+    author_email='tuj455346@temple.edu',
+    description='',
+    install_requries=['torch', 'torch-vision'],
+    package_data={'data': ['*.txt']}
+)
